e61aa3467e1a7534eaeb14f64a99371f2a071896
Brothers <nt84007@gmail.com>
2021-04-23 17:18:11 +0530

    Test Commit by neeraj

--- app/src/main/java/com/example/ugshop/MainActivity2.java
+++ app/src/main/java/com/example/ugshop/MainActivity2.java
@@ -20,12 +20,7 @@ public class MainActivity2 extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
     }
 }
