35133b4b33891bef18e0383fac0aa8be75cde4c0
MandeepKumar1 <money9968@gmail.com>
2021-07-14 00:55:59 +0530

    Added Changes for navigation drawer on HomePage

--- app/src/main/java/com/example/ugshop/view/HomePage.java
+++ app/src/main/java/com/example/ugshop/view/HomePage.java
@@ -1,22 +1,54 @@
 package com.example.ugshop.view;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.ugshop.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomePage extends AppCompatActivity implements View.OnClickListener {
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            //                int id = MenuItem.getItemId();
+//                switch (id) {
+//                    case R.id.home:
+//                        break;
+//                    case R.id.search:
+//                        break;
+//                    case R.id.myOrder:
+//                        break;
+//                    case R.id.setting:
+//                        break;
+//                    case R.id.Account:
+//                        break;
+//                    default:
+//                        return true;
+//                }
+            return true;
+        });
         findViewById(R.id.cart).setOnClickListener(this);
-        findViewById(R.id.menu_line).setOnClickListener(this);
 
         makeNetworkCalls();
     }
@@ -40,10 +72,6 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener 
             case R.id.cart:
                 Intent cartIntent = new Intent(this, MyCartEmpty.class);
                 startActivity(cartIntent);
-                break;
-            case R.id.menu_line:
-                Intent accountSettings = new Intent(this, MainMyAccountActivity.class);
-                startActivity(accountSettings);
                 break;
         }
     }
