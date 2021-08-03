256fe56898efddd5ad8a62faafb89c8d32cd40c0
mquibtiya <mquibtiya@microsoft.com>
2021-08-04 02:03:47 +0530

    inflated imgView

--- app/src/main/java/com/example/ugshop/view/adapter/CartAdapter.java
+++ app/src/main/java/com/example/ugshop/view/adapter/CartAdapter.java
@@ -3,6 +3,7 @@ package com.example.ugshop.view.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +13,7 @@ import com.example.ugshop.R;
 import com.example.ugshop.model.common.ProductModel;
 import com.example.ugshop.view.CartItemModel;
 import com.example.ugshop.view.MyCartActivity;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -75,7 +77,9 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartItemViewho
             mTotalOriginalPrice += originalPrice;
             holder.originalPrice.setText("Rs. " + originalPrice);
             holder.quantity.setText(product.getStockQuantity()+"");
-
+            Picasso.get()
+                    .load(product.getUrlModel().getBaseUrl())
+                    .into(holder.productImage);
             holder.removeItemLayout.setOnClickListener(view -> {
                 callRemoveFromCartApi(product);
             });
@@ -95,6 +99,7 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartItemViewho
         int mViewType = -1;
         TextView productName, productPrice, originalPrice, quantity, totalItemsPrice, totalPrice, deliveryAmount, savedAmount;
         ViewGroup removeItemLayout;
+        ImageView productImage;
 
         public CartItemViewholder(@NonNull View itemView) {
             super(itemView);
@@ -104,6 +109,7 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartItemViewho
                 productPrice = itemView.findViewById(R.id.product_price);
                 originalPrice = itemView.findViewById(R.id.cutted_price);
                 quantity = itemView.findViewById(R.id.product_quantity);
+                productImage = itemView.findViewById(R.id.cart_product_img);
                 removeItemLayout = itemView.findViewById(R.id.remove_item_btn);
             } else if (mViewType == CartItemModel.TOTAL_AMOUNT) {
                 totalItemsPrice = itemView.findViewById(R.id.total_items_price);
