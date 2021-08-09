0fde493b4ced77d36bee7f5b1b83c7dfda3e4a89
Brothers <nt84007@gmail.com>
2021-08-09 10:18:44 +0530

    Committe

--- app/src/main/java/com/example/ugshop/view/adapter/CartAdapter.java
+++ app/src/main/java/com/example/ugshop/view/adapter/CartAdapter.java
@@ -66,7 +66,7 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartItemViewHo
             holder.totalItemsPrice.setText("Rs. " + mTotalOriginalPrice);
             holder.totalPrice.setText("Rs. " + mTotalPrice);
             holder.deliveryAmount.setText("Rs. 0");
-            holder.totalCartAmount.setText("Rs. "+(mTotalOriginalPrice - mTotalPrice));
+//            holder.totalCartAmount.setText("Rs. "+(mTotalOriginalPrice - mTotalPrice));
             holder.savedAmount.setText("You saved " + "Rs. " + (mTotalOriginalPrice - mTotalPrice) + " on this order");
         } else {
             final ProductModel product = mProductsList.get(position);
